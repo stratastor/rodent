8c36ea6f9ff5681c62845010b7223c50a1c14c87
Raamsri Kumar <raam@tinkershack.in>
2025-02-03 14:10:42 +0530

    Handle empty pool list json marshal

--- pkg/zfs/pool/pool.go
+++ pkg/zfs/pool/pool.go
@@ -321,8 +321,11 @@ func (p *Manager) List(ctx context.Context) (ListResult, error) {
 		return result, errors.Wrap(err, errors.ZFSPoolList)
 	}
 
-	if err := json.Unmarshal(out, &result); err != nil {
-		return result, errors.Wrap(err, errors.CommandOutputParse)
+	if len(out) > 0 {
+		fmt.Printf("Output: %s\n", string(out))
+		if err := json.Unmarshal(out, &result); err != nil {
+			return result, errors.Wrap(err, errors.CommandOutputParse)
+		}
 	}
 
 	return result, nil
